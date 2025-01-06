21c56071435be68abfea0936e9f0d02b0f98f5f3
Jorge Alencar Ullmann <81653696+jaullmann@users.noreply.github.com>
2025-01-06 16:54:49 -0300

    Updated Input component layout and functions to allow password visibility option

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,26 +1,40 @@
+import { useState } from 'react';
+import { Eye, EyeOff } from 'lucide-react-native';
 import {
+  Button,
+  useStyled,
   FormControl,
   FormControlError,
   FormControlErrorText,
   Input as GluestackInput,
   InputField,
-} from '@gluestack-ui/themed'
+} from '@gluestack-ui/themed';
 
-import { ComponentProps } from 'react'
+import { ComponentProps } from 'react';
 
 type Props = ComponentProps<typeof InputField> & {
-  errorMessage?: string | null
-  isInvalid?: boolean
-  isReadOnly?: boolean
-}
+  errorMessage?: string | null;
+  isInvalid?: boolean;
+  isReadOnly?: boolean;
+  visibilityControl?: boolean;  
+};
 
 export function Input({
   isReadOnly = false,
   errorMessage = null,
   isInvalid = false,
+  visibilityControl = false,
+  secureTextEntry = false,  
   ...props
 }: Props) {
-  const invalid = !!errorMessage || isInvalid
+
+  const [showPassword, setShowPassword] = useState(false);  
+  const styled = useStyled();
+  const invalid = !!errorMessage || isInvalid;
+
+  const changeTextVisibility = () => {
+    setShowPassword(!showPassword);
+  }
 
   return (
     <FormControl isInvalid={invalid} mb="$2" w="$full">
@@ -29,6 +43,8 @@ export function Input({
         h="$12"
         borderWidth="$0"
         borderRadius="$md"
+        alignItems="center"
+        justifyContent="flex-end"
         $focus={{
           borderWidth: 1,
           borderColor: invalid ? '$red700' : '$gray400',
@@ -41,20 +57,39 @@ export function Input({
         opacity={isReadOnly ? 0.5 : 1}
       >
         <InputField
+          h="$full"
           px="$4"
           bg="$gray700"
           color="$gray300"
           fontFamily="$body"
           placeholderTextColor="$gray400"
+          secureTextEntry={!showPassword}
           {...props}
         />
+        {visibilityControl && (
+          <Button 
+            w={20} h={32}            
+            bg="none"
+            position='absolute' 
+            right={16}
+            onPress={changeTextVisibility}
+          >
+            {!showPassword ? (
+              <Eye color={styled.config.tokens.colors.gray300} />
+            ) : (
+              <EyeOff color={styled.config.tokens.colors.gray300} />
+            )}
+          </Button>
+        )}
       </GluestackInput>
 
-      <FormControlError>
-        <FormControlErrorText color="$red700">
-          {errorMessage}
-        </FormControlErrorText>
-      </FormControlError>
+      {errorMessage && (
+        <FormControlError>
+          <FormControlErrorText color="$red700">
+            {errorMessage}
+          </FormControlErrorText>
+        </FormControlError>
+      )}
     </FormControl>
-  )
+  );
 }
